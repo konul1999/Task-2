2ceb7a3858f7537164570f4b59e327d5e0092b47
ASUS <ASUS@DESKTOP-BG74R8U>
2023-11-24 09:33:57 -0800

    Add project files.

---
+++ Task 2/Program.cs
@@ -0,0 +1,39 @@
+﻿string Name = "Konul";
+string Surname = "Meherremzade";
+
+Console.WriteLine(Name + " " + Surname);
+
+
+
+
+
+
+int a = 3;
+int b = 5;
+int c = a + b;
+
+Console.WriteLine(c);
+
+
+
+
+
+int d = 3;
+int e = 5;
+int f = d - e;
+
+Console.WriteLine(f);
+
+
+
+int g = 2;
+int h = 3;
+int i = g * h;
+int j = i * i;
+Console.WriteLine(j);
+
+
+
+
+double x = (double)18 / 5;
+Console.WriteLine(x * x);
